f8ed8a9ed120a1924f5b4167acf50b93e5073d6b
devi <devi272921@gmail.com>
2024-02-04 20:17:29 +0530

    Updated email service impl and logo for reset email

--- src/main/java/com/springboot/emailutils/Emailserviceimpl.java
+++ src/main/java/com/springboot/emailutils/Emailserviceimpl.java
@@ -44,7 +44,7 @@ public class Emailserviceimpl implements EmailService {
 	        mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
 	        mimeMessageHelper.setFrom(sender);
 	        mimeMessageHelper.setTo(recipient);
-	        mimeMessageHelper.setSubject("Password Reset Mail from Smart Contact Manager");
+	        mimeMessageHelper.setSubject("Password Reset Mail from Effecient Contact Organizer");
 
 	        // Read the Thymeleaf HTML template
 //	        ClassPathResource resource = new ClassPathResource("templates/email.html");
